0d32b6e4037855a8008a5c231cdbe3efbedb014f
BingusEx <10480438+BingusEx@users.noreply.github.com>
2026-04-28 21:17:49 +0200

    Add clamp to gravity adjustment

--- src/hdtSkinnedMesh/hdtSkinnedMeshWorld.cpp
+++ src/hdtSkinnedMesh/hdtSkinnedMeshWorld.cpp
@@ -213,7 +213,8 @@ namespace hdt
 			for (auto& j : i->m_bones) {
 				auto body = &j->m_rig;
 				if (!body->isStaticOrKinematicObject() && !(body->getFlags() & BT_DISABLE_WORLD_GRAVITY)) {
-					body->setGravity(m_gravity * j->m_gravityFactor * j->m_currentTransform.getScale());
+					//Scale gravity based on a rig's scale. Will break past 800x
+					body->setGravity(m_gravity * j->m_gravityFactor * std::min(100.f, j->m_currentTransform.getScale()));
 				}
 			}
 		}
